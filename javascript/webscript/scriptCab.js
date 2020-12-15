6636aee9d10b003fcf5a1353688e4138a50eb2cf
Li WanChang <wanchang.li@pactera.com>
2020-12-15 15:08:24 +0800

    up

--- javascript/webscript/scriptCab.js
+++ javascript/webscript/scriptCab.js
@@ -10,7 +10,7 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
     let urlArr = window.location.host.split(".");
     let atType = urlArr[1];
@@ -19,9 +19,9 @@
     // 判断文章类型
     const trimMd = () => {
         let articleElement;
-        if(atType === "weixin"){
+        if (atType === "weixin") {
             articleElement = document.querySelector("#js_article");
-        }else if(atType === "jianshu"){
+        } else if (atType === "jianshu") {
             articleElement = document.querySelector("#__next > div._21bLU4._3kbg6I > div > div._gp-ck");
         }
         console.clear();
@@ -39,12 +39,12 @@
         while (child) {
             // 标题
             if (child.tagName.length === 2 && child.tagName.charAt(0) === 'H' && !isNaN(+child.tagName.charAt(1)) && !isNothing(child.innerText)) {
-                str += "###########".slice(0,+child.tagName.charAt(1)) + child.innerText + "\n";
+                str += "###########".slice(0, +child.tagName.charAt(1)) + " " + child.innerText + "\n";
             }
             // 文字
             else if (child.tagName === 'P' || child.tagName === 'SPAN' && !isNothing(child.innerText)) {
                 str += child.innerText + "\n";
-                if(child.firstElementChild && child.firstElementChild.tagName === 'IMG'){
+                if (child.firstElementChild && child.firstElementChild.tagName === 'IMG') {
                     str += exEm(child);
                 }
             }
@@ -52,12 +52,12 @@
             else if (child.tagName === 'IMG') {
                 // 简书文章中的图片
                 let imgUrl = "";
-                if(atType === "weixin"){
-                    imgUrl = !isNothing(child.src)&&child.src.slice(0,1)==='h'?child.src:child.dataset.src;
-                }else if(atType === "jianshu"){
-                    imgUrl = !isNothing(child.src)?child.src:"https://"+child.dataset.originalSrc;
+                if (atType === "weixin") {
+                    imgUrl = !isNothing(child.src) && child.src.slice(0, 1) === 'h' ? child.src : child.dataset.src;
+                } else if (atType === "jianshu") {
+                    imgUrl = !isNothing(child.src) ? child.src : "https://" + child.dataset.originalSrc;
                 }
-                str += "![image.png]("+ imgUrl + " \"image\")\n";
+                str += "![image.png](" + imgUrl + " \"image\")\n";
             }
             // 列表
             else if (child.tagName === 'LI' && !isNothing(child.innerText)) {
@@ -78,12 +78,12 @@
     }
 
     // 没有内容
-    const isNothing = str =>{
-        if(str == null) return true;
+    const isNothing = str => {
+        if (str == null) return true;
         //[]内输入你要过滤的字符
         const regExp = new RegExp("[`~%!@#^=''?~！@#￥……&——‘”“'？*()（），,。.、\r\n]");
         //去除
-        str = str.replace(regExp,"");
+        str = str.replace(regExp, "");
         return str.length === 0;
     }
     // 延时执行
